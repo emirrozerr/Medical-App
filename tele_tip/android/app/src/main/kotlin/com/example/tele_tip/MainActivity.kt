d790cf07d3e0fcb3aeb121618c5201e1401e6d90
emirrozerr <emirrozerr@hotmail.com>
2023-01-08 00:20:15 +0300

    fix

--- tele_tip/android/app/src/main/kotlin/com/example/tele_tip/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tele_tip
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- tele_tip/server/app/models/major_science.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Major_science = sequelize.define("major_science", {
-      name: {
-        type: Sequelize.STRING
-      }
-    });
-  
-    return Major_science;
-  };
