357e57052449960378a82bd0fbc0406f755dbdc8
Zeynep Altun <20060325@stu.omu.edu.tr>
2023-01-12 12:21:57 +0300

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:tele_tip/ui/homePage/home_page.dart';
 import 'package:tele_tip/ui/loginPage/login.dart';
 import 'package:tele_tip/ui/registerPage/register_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const MaterialApp(home: MyApp()));
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -13,11 +17,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       routes: {
-        '/': (context) => LoginPage(),
-        '/home': (context) => const RolePage(),
+        '/': (context) => const LoginPage(),
         '/login': (context) => const LoginPage(),
         '/register': (context) => const RolePage(),
+        '/patientHome': (context) => const HomePage(),
+        '/doctorHome': (context) => const HomePage(),
       },
     );
   }
